9847b0814971b1817f9d89bef557fb1ce4b58d8a
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-09-17 01:30:01 +0300

    Add new Folder

--- Syntax, Functions and Statements/02.GreatestCommonDevisor.js
+++ Syntax, Functions and Statements/02.GreatestCommonDevisor.js
@@ -1,1 +1,12 @@
-function
+function greatestCommonDivisior(num1, num2){
+    let divisior;
+    for(let i = num1 ; i > 0 ; i-- ){
+        if(num1 % i == 0 && num2 % i == 0){
+            divisior = i;
+            break;
+        }
+    }
+    return divisior;
+}
+
+console.log(greatestCommonDivisior(15, 5));
