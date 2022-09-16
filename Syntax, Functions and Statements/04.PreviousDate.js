853f87eb2d8ad44451e04dde06c5519f95a1506a
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-09-16 10:50:50 +0300

    Add new Folder

--- Syntax, Functions and Statements/04.PreviousDate.js
+++ Syntax, Functions and Statements/04.PreviousDate.js
@@ -1,6 +1,7 @@
 function previousDay(year, mounth, day){
-    let currentDate = new Date(year, mounth-1, day-1);
-   
+    
+   let currentDate = new Date(year, mounth-1, day-1);
+
     console.log(`${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`)
 }
 
