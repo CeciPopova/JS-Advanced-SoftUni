916e999f668500718bec5445daaadb851a47cdb1
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-09-21 23:30:39 +0300

    Add new folder

---
+++ Arrays/Arrays-Lab/02.LastKNumbersSequence.js
@@ -0,0 +1,17 @@
+function solve(n,k){
+
+    let arr = [1];
+    
+    for (let i = 1; i < n; i++) {
+      let startIndex = i - k ;
+      if (startIndex < 0) {
+        startIndex = 0;
+      }else{
+        startIndex = i - k;
+      }
+      arr.push(arr.slice(startIndex).reduce((acc, num)=> acc + num));
+
+    }
+    return arr;
+}
+console.log(solve(6,3));
