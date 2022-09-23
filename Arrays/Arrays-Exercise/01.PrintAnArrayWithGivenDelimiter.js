ea6765f52dc832d18ac49f310efebda56c92bb66
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-09-23 12:30:26 +0300

    Add new Folder

---
+++ Arrays/Arrays-Exercise/01.PrintAnArrayWithGivenDelimiter.js
@@ -0,0 +1,12 @@
+function solve(input, delimiter) {
+
+    let result = input.join(delimiter);
+    return result
+}
+console.log(solve(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-'
+));
