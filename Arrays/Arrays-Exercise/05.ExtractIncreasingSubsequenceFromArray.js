ba5d62276fd8b05bdc8dcab7ed05199d3dc0f855
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-09-24 13:38:42 +0300

    Complete solution

--- Arrays/Arrays-Exercise/05.ExtractIncreasingSubsequenceFromArray.js
+++ Arrays/Arrays-Exercise/05.ExtractIncreasingSubsequenceFromArray.js
@@ -1,17 +1,20 @@
-function solve(input) {
+function solve(array) {
+
+    let biggestNum = array[0];
     let result = [];
-    let biggestNum = 0;
     
-    for (let i = 0; i < input.length; i++) {
-       if (input[i] > biggestNum) {
-        result.push(input[i]);
-        biggestNum = input[i];
-       }
-    }
+    array.reduce((acc, el) => {
+        if (el >= biggestNum) {
+            biggestNum = el;
+            acc.push(el);
+        }
+
+        return acc;
+    }, result);
+
     return result;
 }
-console.log(solve([1, 
-    2, 
-    3,
-    4]
-    ))
+
+console.log(solve([1, 3, 8, 4, 10, 12, 3, 2, 24]));
+console.log(solve([1, 2, 3, 4]));
+console.log(solve([20, 3, 2, 15, 6, 1]));
