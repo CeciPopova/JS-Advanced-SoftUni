31645c9742d9d90e2f634765792f193098fe0cc3
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-10-08 00:45:05 +0300

    Modified a solution

--- 04. DOM - Introduction/DOM-Exersice/02. Pascal or Camel Case/solution.js
+++ 04. DOM - Introduction/DOM-Exersice/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,47 @@
 function solve() {
-  //TODO...
+  let textElement = document.getElementById('text').value;
+  let convention = document.getElementById('naming-convention').value;
+  let result ='';
+
+ if (convention === 'Camel Case') {
+    let text = textElement.toLowerCase();
+ 
+    for (let index = 0; index < text.length; index++) {
+      if(text[index]=== ' '){
+    
+        result += text[index + 1].toUpperCase();
+        index++;
+      }else{
+        result += text[index];
+      }
+    }
+  }else if(convention === 'Pascal Case'){
+    let text = textElement.toLowerCase();
+   
+    for (let index = 0; index < text.length; index++) {
+      
+      if(text[index]=== ' '){
+    
+        result += text[index + 1].toUpperCase();
+        index++;
+      }else{
+        if (index === 0) {
+         
+          result += text[index].toUpperCase();
+        }else{
+          result += text[index];
+        }
+       
+      }
+    }
+
+  }else{
+    result = 'Error!';
+  }
+
+
+ let resultElement = document.getElementById('result');
+ resultElement.textContent = result;
+ return resultElement;
+
 }
