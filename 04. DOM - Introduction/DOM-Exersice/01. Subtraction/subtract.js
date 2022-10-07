e6938a1edd52e1252e1a8d671e864956f6483fe7
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-10-07 23:38:49 +0300

    Add new Folder

---
+++ 04. DOM - Introduction/DOM-Exersice/01. Subtraction/subtract.js
@@ -0,0 +1,12 @@
+function subtract() {
+    let firstNumberElement = document.getElementById('firstNumber');
+    let secondNumberElement = document.getElementById('secondNumber');
+
+    let firstNum = Number(firstNumberElement.value);
+    let secondNum = Number(secondNumberElement.value);
+
+    let result = firstNum - secondNum;
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = result;
+    console.log(resultElement);
+}
