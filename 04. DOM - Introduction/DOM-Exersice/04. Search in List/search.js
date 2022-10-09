098c6b4583054e1616b4ecf3a6171030fcac3110
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-10-10 01:12:50 +0300

    Modified Files

--- 04. DOM - Introduction/DOM-Exersice/04. Search in List/search.js
+++ 04. DOM - Introduction/DOM-Exersice/04. Search in List/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+   let elementsToSearch = Array.from(document.querySelectorAll('#towns li'));
+   let textToSearch = document.querySelector('#searchText').value;
+   let count = 0;
+
+   for (const el of elementsToSearch) {
+      if (el.textContent.includes(textToSearch) && textToSearch !== '') {
+         el.style.fontWeight='bold';
+         el.style.textDecoration = 'underline';
+         count++;
+      } else {
+         el.style.fontWeight='normal';
+         el.style.textDecoration = 'none';
+      }
+   }
+   let maches = document.getElementById('result');
+   maches.textContent = `${count} matches found`;
+
+
 }
--- 04. DOM - Introduction/DOM-Exersice/05. Table - Search Engine/solution.js
+++ 04. DOM - Introduction/DOM-Exersice/05. Table - Search Engine/solution.js
@@ -1,8 +1,15 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let input = document.getElementById('searchField');
+   let rows = document.querySelectorAll('tbody tr');
 
    function onClick() {
-      //   TODO:
-
+      for (let row of rows) {
+         row.classList.remove('select');
+         if (row.innerHTML.includes(input.value) && input.value !== '') {
+            row.className = 'select';
+         }
+      }
+      input.value = '';
    }
 }
