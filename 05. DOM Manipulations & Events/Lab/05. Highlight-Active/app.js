56c299829aecc25868b2a4d36bf837216211c9d0
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-10-13 23:36:00 +0300

    Complete solutions

--- 05. DOM Manipulations & Events/Lab/05. Highlight-Active/app.js
+++ 05. DOM Manipulations & Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+
+    let sectionElements = document.querySelectorAll('input');
+
+
+    for (const el of sectionElements) {
+        el.addEventListener('focus', (e) => {
+            e.target.parentNode.classList.add('focused')
+        });
+        el.addEventListener('blur', (e) => {
+            e.target.parentNode.classList.remove('focused');
+        });
+    }
 }
--- 05. DOM Manipulations & Events/Lab/06. Dynamic-Validation/app.js
+++ 05. DOM Manipulations & Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('email');
+    let regex = /[a-z]+@[a-z]+\.[a-z]+/;
+
+   
+    inputElement.addEventListener('change', (e) => {
+        if (!regex.test(e.currentTarget.value)) {
+            e.currentTarget.className = 'error';
+        } else {
+            e.currentTarget.className = '';
+        }
+    });
 }
