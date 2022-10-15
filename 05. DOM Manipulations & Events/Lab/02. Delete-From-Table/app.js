44ae53e794ff30b2362473e3103768da5e6bb6c0
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-10-15 23:13:34 +0300

    Modified Files

--- 05. DOM Manipulations & Events/Lab/02. Delete-From-Table/app.js
+++ 05. DOM Manipulations & Events/Lab/02. Delete-From-Table/app.js
@@ -1,16 +1,27 @@
 function deleteByEmail() {
-    let emailInputElement = document.querySelector('input[name="email"]');
-    let tableElements = document.querySelectorAll('tr td:nth-of-type(2)');
+    // let emailInputElement = document.querySelector('input[name="email"]');
+    // let tableElements = document.querySelectorAll('tr td:nth-of-type(2)');
+    // let resultElement = document.getElementById('result');
+    // let emailElement = Array.from(tableElements);
+    // let targetElement = emailElement.find(x => x.textContent == emailInputElement.value);
 
-    let resultElement = document.getElementById('result');
+    // if (targetElement) {
+    //     targetElement.parentNode.remove();
+    //     resultElement.textContent = 'Deleted.';
+    // } else {
+    //     resultElement.textContent = 'Not found.';
+    // }
 
-    let emailElement = Array.from(tableElements);
-    let targetElement = emailElement.find(x => x.textContent == emailInputElement.value);
-    if (targetElement) {
-        targetElement.parentNode.remove();
-        resultElement.textContent = 'Deleted.';
-    } else {
-        resultElement.textContent = 'Not found.';
+   let infoElements = Array.from(document.querySelectorAll('tr td'));
+   let inputElement = document.querySelector('input[name="email"]');
+   let result = document.getElementById('result');
+   for (i = 0 ; i < infoElements.length ; i++) {
+    if (infoElements[i].textContent == inputElement.value) {
+        infoElements[i].parentNode.remove();
+        result.textContent = 'Deleted.'
+       
+    }else{
+        result.textContent = 'Not found.';
     }
-   
+   }
 }
--- 05. DOM Manipulations & Events/Lab/03. Add-Delete/app.js
+++ 05. DOM Manipulations & Events/Lab/03. Add-Delete/app.js
@@ -1,20 +1,21 @@
 function addItem() {
-    //TODO...
-
-    let itemsElement = document.getElementById('items');
-    let inputElement = document.getElementById('newItemText');
-
-    let liElement = document.createElement('li');
-    liElement.textContent = inputElement.value;
-    inputElement.value = '';
+   
+    let input = document.getElementById('newItemText');
+    let li = document.createElement('li');
+    let items = document.getElementById('items');
+    li.textContent = input.value;
+    input.value = '';
 
     let deleteElement = document.createElement('a');
     deleteElement.href = '#';
     deleteElement.textContent = '[Delete]';
-    deleteElement.addEventListener('click', (e) =>{
-        e.currentTarget.parentElement.remove();
-    });
 
-    liElement.appendChild(deleteElement);
-    itemsElement.appendChild(liElement);
+    deleteElement.addEventListener('click', deleteFunction);
+
+    function deleteFunction(ev) {
+        ev.target.parentElement.remove();
+    }
+
+    li.appendChild(deleteElement);
+    items.appendChild(li);
 }
