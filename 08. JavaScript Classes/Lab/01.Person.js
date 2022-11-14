69d3b0b1c0ec1a79b9cd53a2dc80b76588a4746f
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-11-14 12:34:43 +0200

    Modified folder

--- 08. JavaScript Classes/Lab/01.Person.js
+++ 08. JavaScript Classes/Lab/01.Person.js
@@ -1,17 +1,15 @@
-class Person {
-    constructor (firstName, lastName, age , email) {
+class Person{
+    constructor(firstName, lastName, age, email){
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.email = email;
-
     }
-    toString() {
-        let personalInfo = `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
-        return personalInfo;
+
+    toString(){
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
     }
 }
-
 
 let person = new Person ('Ana', 'Simpson', 23, 'anas@yahoo.com');
 console.log(person.toString());
